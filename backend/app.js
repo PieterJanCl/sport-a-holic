da97c46dfde1177db6d521897dfeeb3cdd7e1ded
PieterJanCl <pieterjan.clijsters@gmail.com>
2017-12-07 18:45:16 +0100

    add databank link to databank to project

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+const port = '3000';
+
+mongoose.connect('mongodb://localhost/sportdb', {  useMongoClient: true });
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -42,5 +47,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port, () => {
+  console.log("App listening at port: " + port)
+})
 
 module.exports = app;
