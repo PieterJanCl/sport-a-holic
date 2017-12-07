d1b1c5b6910a55cb98ce6e6a1d515562e28c53c5
PieterJanCl <pieterjan.clijsters@gmail.com>
2017-12-07 14:53:11 +0100

    add mongoose

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/API/register/', function(req, res, next) {
+  res.send("process the request here");
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
